ce46df1b58d897675faadd486a040afaacedf3d1
Gabriele Mario Tosto <gabriele.tosto@outlook.com>
2024-01-24 13:09:29 +0100

    Fixata l'accessibilità per hamburger menu

--- scripts/hamburger.js
+++ scripts/hamburger.js
@@ -1,5 +1,5 @@
 const hamburgerMenu = document.querySelector('header .header__wrapper .mobile__only');
-const hamburgerMenuClose = document.querySelector('header nav.mobile img');
+const hamburgerMenuClose = document.querySelector('header nav.mobile .close__menu');
 const navList = document.querySelector('header nav.mobile');
 
 
@@ -8,16 +8,20 @@ hamburgerMenuClose.addEventListener('click', closeMenu);
 
 function openMenu () {
   navList.style.display = "block";
+  hamburgerMenu.ariaPressed = "true";
   setTimeout(() => {
     navList.classList.add('show__mobile');
     hamburgerMenu.classList.add('show__mobile');
+    hamburgerMenuClose.focus();
   }, 100);
 }
 
 function closeMenu () {
   navList.classList.remove('show__mobile');
+  hamburgerMenu.ariaPressed = "false";
   setTimeout(() => {
     hamburgerMenu.classList.remove('show__mobile');
     navList.style.display = "none";
+    hamburgerMenu.focus();
   }, 600)
 }
