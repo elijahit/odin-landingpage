e0150703aeacbeba6f2868a58b40a4fa051b4196
Gabriele Mario Tosto <gabriele.tosto@outlook.com>
2024-01-24 15:31:14 +0100

    Gesiste tramite header.js le casistiche di index/pagine
    E' stato necessario gestire la baseUrl per le pagine diverse da index
    perchè non riuscivano a riprendere le icone per l'hamburger menu.
    
    E' stato necessario gestire la classe nav__active tramite JS per
    fargli selezionare tramite un forEach l'elemento li a cui assegnare
    la classe active__nav che deve essere presente nel file CSS della
    pagina come contact.css è presente per contact.html.

--- scripts/default_components/header.js
+++ scripts/default_components/header.js
@@ -1,29 +1,58 @@
-document.write('\
+let baseUrl;
+
+// GESTISCO L'URL BASE PER INDEX e PAGINE VARIE 
+if (window.location.href.includes('index.html')) {
+  baseUrl = ".";
+} else {
+  baseUrl = "..";
+}
+
+// INSERISCO L'HTML dell'HEADER CHE HA BISOGNO DI
+// criticals.css PER FUNZIONARE
+document.write(`\
   <header>\
     <div class="header__wrapper">\
       <div class="logo" aria-labelledby="logo__site">\
-        <a id="logo__site" href="./index.html">Header Logo</a>\
+        <a id="logo__site" href="${baseUrl}/index.html">Header Logo</a>\
       </div>\
       <button class="mobile__only" aria-label="navigazione principale" aria-pressed="false">\
-        <img src="./images/hamburger.svg" alt="Menu Principale" />\
+        <img src="${baseUrl}/images/hamburger.svg" alt="Menu Principale" />\
       </button>\
       <nav class="desktop" aria-label="principale">\
         <ul>\
           <li><a href="#">header link one</a></li>\
           <li><a href="#">header link two</a></li>\
-          <li><a href="./pages/contact.html">Contact</a></li>\
+          <li><a href="${baseUrl}/pages/contact.html">Contact</a></li>\
         </ul>\
       </nav>\
     </div>\
     <nav class="mobile" aria-label="principale">\
       <button class="close__menu" aria-label="navigazione principale" aria-pressed="false">\
-        <img src="./images/closeHamburger.svg" alt="Chiudi menu">\
+        <img src="${baseUrl}/images/closeHamburger.svg" alt="Chiudi menu">\
       </button>\
       <ul>\
         <li><a href="#">header link one</a></li>\
         <li><a href="#">header link two</a></li>\
-        <li><a href="./pages/contact.html">Contact</a></li>\
+        <li><a href="${baseUrl}/pages/contact.html">Contact</a></li>\
       </ul>\
     </nav>\
   </header>\
-');
+`);
+
+// DOPO AVER INSERITO L'HTML SELEZIONO L'ELEMENTO DA ATTIVARE
+// SE SI E' IN UNA PAGINA DIVERSA DA INDEX.HTML TRAMITE
+// DUE FOREACH VALUTO SE DESKTOP O MOBILE SONO SELEZIONATI
+// NECESSITA DI UNA CLASSE active__nav IMPOSTATA NEL CSS
+//DELLA PAGINA COME AD ESEMPIO CONTACT.CSS PER PAGES/CONTACT.HTML
+
+document.querySelectorAll('header nav.mobile ul li').forEach((e) => {
+  if(e.innerHTML.includes(window.location.href.split("/")[window.location.href.split("/").length-1])) {
+    e.classList.add('active__nav');
+  }
+});
+
+document.querySelectorAll('header .header__wrapper nav.desktop ul li').forEach((e) => {
+  if(e.innerHTML.includes(window.location.href.split("/")[window.location.href.split("/").length-1])) {
+    e.classList.add('active__nav');
+  }
+});
